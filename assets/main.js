38d827991a466fe805fd51cbbc6b760234c477c2
PedroCrtz <133224989+PedroCrtz@users.noreply.github.com>
2024-11-04 11:45:27 -0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,19 @@
-function 
+function criaCartao(tema, pergunta, reposta) {
+    const container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="cartao-conteudo">
+                    <h3>${tema}</h3>
+                    <div class="cartao-conteudo-pergunta">
+                       ${pergunta}
+                    </div>
+                    <div class="cartao-conteudo-resposta">
+                        ${reposta}
+                    </div>
+                </div>
+                `
+    container.appendChild(cartao)
+}
+criaCartao('Historia', 'Em que ano se iniciou-se guerra do vietnã?', 'em 1955')
+criaCartao('comedia', 'por que a galinha atravesou a rua?','para chegar ao outro lado')
